4ce7a4c0d4cd4b2e3d02bc5dffce96b5358b51c5
Chan Zhe Xiang <czhexg1@gmail.com>
2025-05-20 10:18:04 +0800

    feat: add services and controllers to container for dependency injection

--- services/post-service/src/container.ts
+++ services/post-service/src/container.ts
@@ -1,6 +1,12 @@
+import { CommentController } from "./controllers/comment.controller";
+import { LikeController } from "./controllers/like.controller";
+import { PostController } from "./controllers/post.controller";
 import { CommentRepository } from "./repositories/comment.repository";
 import { LikeRepository } from "./repositories/like.repository";
 import { PostRepository } from "./repositories/post.repository";
+import { CommentService } from "./services/comment.service";
+import { LikeService } from "./services/like.service";
+import { PostService } from "./services/post.service";
 
 export class Container {
     private static instances: { [key: string]: any } = {};
@@ -24,5 +30,61 @@ export class Container {
             this.instances.commentRepository = new CommentRepository();
         }
         return this.instances.commentRepository;
+    }
+
+    static getPostService(): PostService {
+        if (!this.instances.postService) {
+            this.instances.postService = new PostService(
+                this.getPostRepository()
+            );
+        }
+        return this.instances.postService;
+    }
+
+    static getLikeService(): LikeService {
+        if (!this.instances.likeService) {
+            this.instances.likeService = new LikeService(
+                this.getLikeRepository(),
+                this.getPostRepository()
+            );
+        }
+        return this.instances.likeService;
+    }
+
+    static getCommentService(): CommentService {
+        if (!this.instances.commentService) {
+            this.instances.commentService = new CommentService(
+                this.getCommentRepository(),
+                this.getPostRepository()
+            );
+        }
+        return this.instances.commentService;
+    }
+
+    static getPostController(): PostController {
+        if (!this.instances.postController) {
+            this.instances.postController = new PostController(
+                this.getPostService()
+            );
+        }
+        return this.instances.postController;
+    }
+
+    static getLikeController(): LikeController {
+        if (!this.instances.likeController) {
+            this.instances.likeController = new LikeController(
+                this.getLikeService()
+            );
+        }
+        return this.instances.likeController;
+    }
+
+    static getCommentController(): CommentController {
+        if (!this.instances.commentController) {
+            this.instances.commentController = new CommentController(
+                this.getCommentService()
+            );
+        }
+        return this.instances.commentController;
     }
 }
